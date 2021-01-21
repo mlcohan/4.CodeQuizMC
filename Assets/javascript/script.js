a120d5ba72dade248c2aff409cad20dc6a90a294
Mary Cohan <mary.cohan@gmail.com>
2021-01-20 18:12:21 -0800

    going round in circles

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -1,28 +1,35 @@
 var timerElement = document.querySelector(".timer-count");
+var timeWithText = document.getElementById("timer-text")
 // var startButton = document.querySelector(".start-button");
 // var quizQuestion = document.querySelector(".question");
 
-//selecting =elements on html
+//selecting elements on html
 var questionContainer = document.getElementById('question-container')
 var startButton = document.getElementById('start-btn')
 var nextButton = document.getElementById('next-btn')
+var storeScoreButton = document.getElementById('score-btn')
 var questionElement = document.getElementById('question')
 var answerButtonsElement = document.getElementById('answer-buttons')
 
-// var chosenQuestion = "";
-// var timer;
-// var timerCount;
-// var quizComplete;
+
+//defining timer variables 
+var timer;
+var timerCount;
+var quizComplete;
 
 //variable to shuffle questions
-var shuffledQuestions
+var shuffledQuestions 
 var currentQuestionIndex
 
+//question bank
  var questions = [
    {
      question: "What does DOM stand for?", 
      answers: [
        { text: "idk", correct: true },
+       { text: "idk", correct: false },
+       { text: "idk", correct: false },
+       { text: "idk", correct: false },
        { text: "ugh", correct: false }
      ]
     },
@@ -30,6 +37,9 @@ var currentQuestionIndex
       question: "What does Any of this mean?", 
       answers: [
         { text: "idk", correct: true },
+        { text: "idk", correct: false },
+        { text: "idk", correct: false },
+        { text: "idk", correct: false },
         { text: "ugh", correct: false }
       ]
      },
@@ -37,6 +47,9 @@ var currentQuestionIndex
       question: "What YA THNINK?", 
       answers: [
         { text: "idk", correct: true },
+        { text: "idk", correct: false },
+        { text: "idk", correct: false },
+        { text: "idk", correct: false },
         { text: "ugh", correct: false }
       ]
      },
@@ -45,6 +58,8 @@ var currentQuestionIndex
       question: "Whats really good?", 
       answers: [
         { text: "idk", correct: true },
+        { text: "idk", correct: false },
+        { text: "idk", correct: false },
         { text: "ugh", correct: false }
       ]
      },
@@ -53,33 +68,39 @@ var currentQuestionIndex
 
 
 // Attach event listener to start button to call startGame function on click
+storeScoreButton.addEventListener("click", storeScore)
 startButton.addEventListener("click", startGame)
 nextButton.addEventListener("click", () => {
   currentQuestionIndex++
   setNextQuestion()
 })
 
+
+//Start Game Function
 function startGame() {
 
 // document.getElementById("start-button").style.visibility = "hidden";
-    // timerCount = 60;
-    // startTimer()
+    timerCount = 60;
+    startTimer()
     renderQuestions()
     startButton.classList.add('hide')
     shuffledQuestions = questions.sort(() => Math.random - .5) //shuffles questions
     currentQuestionIndex = 0
     questionContainer.classList.remove('hide')
+    timeWithText.classList.remove('hide')
     setNextQuestion()
   }
 
 
 //do something with quizComplete
 
+//when the play/next button is clicked
 function setNextQuestion (){
   resetState()
   showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
+//show the next question
 function showQuestion(question) {
   questionElement.innerText = question.question
   question.answers.forEach(answer => {
@@ -95,7 +116,7 @@ function showQuestion(question) {
   })
 }
 
-//clears the page 
+//clears the page of  old Elements
 function resetState() {
   clearStatusClass(document.body)
   nextButton.classList.add("hide")
@@ -105,6 +126,9 @@ function resetState() {
   }
 }
 
+
+
+//Selecting the answer function
 function selectAnswer(e) {
   var selectedButton = e.target
   var correct = selectedButton.dataset.correct
@@ -112,11 +136,11 @@ function selectAnswer(e) {
   Array.from(answerButtonsElement.children).forEach(button => {
     setStatusClass(button, button.dataset.correct)
   })
-  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+  if (gameComplete = shuffledQuestions.length > currentQuestionIndex + 1) {
   nextButton.classList.remove('hide')
 } else {
-  startButton.innerText = "Restart"
-  startButton.classList.remove('hide')
+  resetState()
+  winGame()
 }
 }
 
@@ -126,6 +150,7 @@ function setStatusClass(element, correct) {
     element.classList.add("correct")
   } else {
     element.classList.add("wrong")
+    timerCount --
   }
 }
 
@@ -134,14 +159,16 @@ function clearStatusClass(element) {
   element.classList.remove("wrong")
 }
 
-// function winGame () {
-//     quizQuestion.textContent = "YOU MADE IT TO THE HALL OF FAME"
+function winGame () {
+  questionContainer.textContent = "YOU WIN!! YOU FINISHED WITH " + timerCount + " seconds left!"
+  storeScoreButton.classList.remove('hide')
+  timeWithText.classList.add('hide');
+  storeScore()
+}
 
-//     function enterIntials(){
-
-//     }
-// }
-
+function storeScore(){
+  storeScoreButton.classList.add("hide")
+}
 // function outtaTime () {
 
 // }
@@ -149,28 +176,36 @@ function clearStatusClass(element) {
 
 
 // The setTimer function starts and stops the timer and triggers winGame() and loseGame()
-// function startTimer() {
-//   // Sets timer
-//   timer = setInterval(function() {
-//     timerCount--;
-//     timerElement.textContent = timerCount;
-//     if (timerCount >= 0) {
-//         // Tests if win condition is met
-//         if (quizComplete && timerCount > 0) {
-//           // Clears interval and stops timer
-//           clearInterval(timer);
-//           winGame();
-//         }
-//       }
-//       // Tests if time has run out
-//       if (timerCount === 0) {
-//         // Clears interval
-//         clearInterval(timer);
-//         outtaTime();
-//       }
+function startTimer() {
+  // Sets timer
+  timer = setInterval(function() {
+    timerCount--;
+    timerElement.textContent = timerCount;
+    if (timerCount >= 0) {
+        // Tests if win condition is met
+        if (quizComplete && timerCount > 0) {
+          // Clears interval and stops timer
+          clearInterval(timer);
+          winGame();
+        }
+      }
+      // Tests if time has run out
+      if (timerCount <= 0) {
+        // Clears interval
+        clearInterval(timer);
+        outtaTime();
+      }
   
-//   }, 1000);
-// }
+  }, 1000);
+}
+
+function outtaTime() {
+  questionContainer.textContent = "YOU LOSE! TRY AGAIN!"
+  timeWithText.classList.add("hide");
+  resetState()
+
+}
+
 
 function renderQuestions() {
     chosenQuestion = questions[Math.floor(Math.random()*questions.length)];
